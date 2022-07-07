ab03203ebadac57d0b1ef3ccac97e7c5a8c6959b
CarlosE3595 <cmejr95@gmail.com>
2022-07-07 00:30:33 -0700

    js starter code

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+var answersList = document.querySelector("#answers-list")
+var highscoresList = document.querySelector("#highscores-list")
+var startQuizBtn = document.querySelector("#start-quiz-btn")
+
+function startGame() {
+
+}
+
+
+function setNextQuestion() {
+
+}
+
+function selectAnswer() {
+
+}
